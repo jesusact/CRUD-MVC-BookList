5b5934a7f9b98947812366368a38add1510a499e
jesusact <jesusaceituno03@gmail.com>
2024-04-09 22:22:21 +0200

    Añadida linea de prueba en index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // https://github.com/keefekwan/php_crud_mvc_oop
 
 require_once 'controller/BooksController.php';
-
+echo 'prueba';
 $controller = new BooksController();
 
 $controller->handleRequest();
